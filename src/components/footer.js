2aa92aaf62be27e8c1c9b82ee08b49b561882cd9
Dan Choiniere <dchoinie@gmail.com>
2019-06-25 12:39:48 -0500

    Update footer.js
    Styled footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,37 +1,32 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
+import Dropdown from "../components/Dropdown"
 
 export class footer extends Component {
   render() {
     return (
-      <div className="footer bg-gray-800 text-white">
-        <div className="row text-center">
-          <div className="">
-            <ul>
-              <li>
-                <Link to="/">Archives</Link>
-              </li>
-              <li>
-                <Link to="/dancelandbandstand">Danceland Bandstand</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="">
-            <p>© {new Date().getFullYear()} | Steve Wilson</p>
-            <small>
-              Website by{" "}
-              <a href="https://github.com/dchoinie" target="_blank">
-                Dan Choiniere
-              </a>
-            </small>
-          </div>
-          <div className="">Footer 3</div>
+      <div className="flex flex-col justify-center bg-gray-800 text-white text-center">
+        <div className="px-2 py-2 flex justify-center">
+          <ul className="flex flex-row">
+            <Dropdown />
+            <li className="px-2">
+              <Link to="/dancelandbandstand">Danceland Bandstand</Link>
+            </li>
+            <li className="px-2">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="px-2">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="px-2 py-2">
+          <p>© {new Date().getFullYear()} | Steve Wilson</p>
+          <small>
+            <a href="https://github.com/dchoinie" target="_blank">
+              Website by Dan Choiniere
+            </a>
+          </small>
         </div>
       </div>
     )
