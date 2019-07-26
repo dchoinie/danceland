43b436c99db3762c9ee2eac92d5540d9b036df0d
Dan Choiniere <dchoinie@gmail.com>
2019-07-26 14:06:06 -0500

    Year Selector Component Redesign

--- src/components/yearSelector.js
+++ src/components/yearSelector.js
@@ -1,115 +1,135 @@
 import React, { Component } from "react"
-import styled from "styled-components"
 import { Link } from "gatsby"
 
 class YearSelector extends Component {
   render() {
     return (
       <>
-        <h2 className="font-bold text-4xl text-center py-16">Search By Year</h2>
-        <div className="container px-4 mx-auto py-8">
-          <div class="flex mb-4 text-center">
-            <div class="w-1/5 bg-gray-900 hover:bg-gray-800">
-              <Link to="/1954">
-                <h2 className="text-lg font-bold py-4 text-red-700">1954</h2>
-              </Link>
+        <h2 className="text-center text-5xl font-bold">Search By Year</h2>
+        <div
+          id="yearSelectorWrapper"
+          className="container mx-auto px-6 text-white my-12 text-center"
+        >
+          <Link
+            to="/1954"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1954</h2>
             </div>
-            <div
-              class="w-1/5 bg-white hover:bg-gray-100"
-              style={{ border: "2px solid #1A202C" }}
-            >
-              <Link to="/1955">
-                <h2 className="text-lg font-bold py-4 text-red-700">1955</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1955"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1955</h2>
             </div>
-            <div class="w-1/5 bg-gray-900 hover:bg-gray-800">
-              <Link to="/1956">
-                <h2 className="text-lg font-bold py-4 text-red-700">1956</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1956"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1956</h2>
             </div>
-            <div
-              class="w-1/5 bg-white hover:bg-gray-100"
-              style={{ border: "2px solid #1A202C" }}
-            >
-              <Link to="/1957">
-                <h2 className="text-lg font-bold py-4 text-red-700">1957</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1957"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1957</h2>
             </div>
-            <div class="w-1/5 bg-gray-900 hover:bg-gray-800">
-              <Link to="/1958">
-                <h2 className="text-lg font-bold py-4 text-red-700">1958</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1958"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1958</h2>
             </div>
-          </div>
-          <div class="flex mb-4 text-center">
-            <div
-              class="w-1/5 bg-white hover:bg-gray-100"
-              style={{ border: "2px solid #1A202C" }}
-            >
-              <Link to="/1959">
-                <h2 className="text-lg font-bold py-4 text-red-700">1959</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1959"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1959</h2>
             </div>
-            <div class="w-1/5 bg-gray-900 hover:bg-gray-800 text-red-700">
-              <Link to="/1960">
-                <h2 className="text-lg font-bold py-4 text-red-700">1960</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1960"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1960</h2>
             </div>
-            <div
-              class="w-1/5 bg-white hover:bg-gray-100"
-              style={{ border: "2px solid #1A202C" }}
-            >
-              <Link to="/1961">
-                <h2 className="text-lg font-bold py-4 text-red-700">1961</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1961"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1961</h2>
             </div>
-            <div class="w-1/5 bg-gray-900 hover:bg-gray-800 text-red-700">
-              <Link to="/1962">
-                <h2 className="text-lg font-bold py-4 text-red-700">1962</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1962"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1962</h2>
             </div>
-            <div
-              class="w-1/5 bg-white hover:bg-gray-100"
-              style={{ border: "2px solid #1A202C" }}
-            >
-              <Link to="/1963">
-                <h2 className="text-lg font-bold py-4 text-red-700">1963</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1963"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1963</h2>
             </div>
-          </div>
-          <div class="flex mb-4 text-center">
-            <div class="w-1/5 bg-gray-900 hover:bg-gray-800">
-              <Link to="/1964">
-                <h2 className="text-lg font-bold py-4 text-red-700">1964</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1964"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1964</h2>
             </div>
-            <div
-              class="w-1/5 bg-white hover:bg-gray-100"
-              style={{ border: "2px solid #1A202C" }}
-            >
-              <Link to="/1965">
-                <h2 className="text-lg font-bold py-4 text-red-700">1965</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1965"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1965</h2>
             </div>
-            <div class="w-1/5 bg-gray-900 hover:bg-gray-800">
-              <Link to="/1966">
-                <h2 className="text-lg font-bold py-4 text-red-700">1966</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1966"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1966</h2>
             </div>
-            <div
-              class="w-1/5 bg-white hover:bg-gray-100"
-              style={{ border: "2px solid #1A202C" }}
-            >
-              <Link to="/1967">
-                <h2 className="text-lg font-bold py-4 text-red-700">1967</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1967"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1967</h2>
             </div>
-            <div class="w-1/5 bg-gray-900 hover:bg-gray-800">
-              <Link to="/1968">
-                <h2 className="text-lg font-bold py-4 text-red-700">1968</h2>
-              </Link>
+          </Link>
+          <Link
+            to="/1968"
+            className="bg-gray-900 text-3xl flex justify-center rounded-lg hover:bg-gray-800 hover:text-red-700"
+          >
+            <div className="self-center">
+              <h2>1968</h2>
             </div>
-          </div>
+          </Link>
         </div>
       </>
     )
